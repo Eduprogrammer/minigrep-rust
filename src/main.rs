2773b2fb93adac0cad93ed2a493b046395fbfbda
Eduprogrammer <educarlos2903@gmail.com>
2025-09-22 21:15:40 -0300

    feat: Inclusao de leitura e busca em dois arquivos (closes #4)

--- src/main.rs
+++ src/main.rs
@@ -3,22 +3,16 @@ use std::process;
 
 use minigrep::Config;
 
-
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problema ao analisar os argumentos: {}", err);
+        eprintln!("Problema ao ler argumentos: {}", err);
         process::exit(1);
     });
 
-    println!("Procurando por: {}", config.query);
-    println!("No arquivo: {}", config.filename);
-
     if let Err(e) = minigrep::run(config) {
-        eprintln!("Erro da aplicação: {}", e);
+        eprintln!("Erro na aplicação: {}", e);
         process::exit(1);
     }
 }
-
- 
